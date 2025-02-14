fd13bef46c335895e10037e298cce0613ee0f0be
Hnf77 <hanifazharramadhan879@gmail.com>
2025-02-14 13:20:48 +0700

    add progress load instalation and optimalization code

--- src/Console/InstallsStislaAdmin.php
+++ src/Console/InstallsStislaAdmin.php
@@ -13,50 +13,70 @@ trait InstallsStislaAdmin
      */
     public function installStislaAdmin()
     {
-        $this->line('Installing dependencies...');
+        $totalSteps = 11;
+        $currentStep = 1;
+
+        $progressBar = $this->output->createProgressBar($totalSteps);
+        $progressBar->setFormat(' %current%/%max% [%bar%] %percent:3s%% %message%');
+
+        $progressBar->setMessage('Starting installation...');
+        $progressBar->start();
 
         // Controllers...
         (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
         (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/stisla/app/Http/Controllers', app_path('Http/Controllers'));
+        $progressBar->setMessage('Controllers installed...');
+        $progressBar->advance();
 
         // Requests...
         (new Filesystem)->ensureDirectoryExists(app_path('Http/Requests'));
         (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/stisla/app/Http/Requests', app_path('Http/Requests'));
+        $progressBar->setMessage('Requests installed...');
+        $progressBar->advance();
 
         // Views...
         (new Filesystem)->ensureDirectoryExists(resource_path('views'));
         (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/stisla/resources/views', resource_path('views'));
+        $progressBar->setMessage('Views installed...');
+        $progressBar->advance();
 
         // Components...
         (new Filesystem)->ensureDirectoryExists(app_path('View/Components'));
         (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/stisla/app/View/Components', app_path('View/Components'));
+        $progressBar->setMessage('Components installed...');
+        $progressBar->advance();
 
-        
         // Routes...
         copy(__DIR__.'/../../stubs/stisla/routes/web.php', base_path('routes/web.php'));
         copy(__DIR__.'/../../stubs/stisla/routes/auth.php', base_path('routes/auth.php'));
+        $progressBar->setMessage('Routes installed...');
+        $progressBar->advance();
 
         // "Dashboard" Route...
         $this->replaceInFile('/home', '/dashboard', resource_path('views/welcome.blade.php'));
         $this->replaceInFile('Home', 'Dashboard', resource_path('views/welcome.blade.php'));
+        $progressBar->setMessage('Dashboard route installed...');
+        $progressBar->advance();
+
 
         // Assets...
-        (new Filesystem)->ensureDirectoryExists(public_path('css'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/stisla/public/css', public_path('css'));
+        $assets = [
+            'css',
+            'fonts',
+            'img',
+            'js',
+            'library',
+        ];
+        foreach ($assets as $asset) {
+            (new Filesystem)->ensureDirectoryExists(public_path($asset));
+            (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/stisla/public/'.$asset, public_path($asset));
+            $progressBar->setMessage('Assets installed...');
+            $progressBar->advance();
+        }
+        $progressBar->setMessage('Finalizing...');
+        $progressBar->finish();
+        $this->line('');
 
-        (new Filesystem)->ensureDirectoryExists(public_path('fonts'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/stisla/public/fonts', public_path('fonts'));
-
-        (new Filesystem)->ensureDirectoryExists(public_path('img'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/stisla/public/img', public_path('img'));
-
-        (new Filesystem)->ensureDirectoryExists(public_path('js'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/stisla/public/js', public_path('js'));
-
-        (new Filesystem)->ensureDirectoryExists(public_path('library'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/stisla/public/library', public_path('library'));
-
-        $this->line('Installing Stisla Admin...');
         $this->components->info('Stisla Admin installed successfully.');
 
     }
