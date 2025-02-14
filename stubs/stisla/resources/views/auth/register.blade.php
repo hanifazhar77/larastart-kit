501ef0dca1aa5134f0e665dad399a6c38a2d9696
Hnf77 <hanifazharramadhan879@gmail.com>
2025-02-14 21:08:27 +0700

    adding new feature tag html custom for auth

--- stubs/stisla/resources/views/auth/register.blade.php
+++ stubs/stisla/resources/views/auth/register.blade.php
@@ -1,81 +1,79 @@
-@extends('layouts.auth')
+<x-auth-layout title="Register">
+    @push('style')
+        <!-- CSS Libraries -->
+        <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+    @endpush
 
-@section('title', 'Register')
+    <x-slot name="content">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>Register</h4>
+            </div>
 
-@push('style')
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
-@endpush
-
-@section('content')
-    <div class="card card-primary">
-        <div class="card-header">
-            <h4>Register</h4>
-        </div>
-
-        <div class="card-body">
-            <form method="POST" action="{{ route('register') }}">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Username</label>
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                        name="name" autofocus>
-                    @error('name')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                        name="email">
-                    @error('email')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <label for="password" class="d-block">Password</label>
-                    <input id="password" type="password" class="form-control pwstrength" data-indicator="pwindicator"
-                        name="password">
-                    <div id="pwindicator" class="pwindicator">
-                        <div class="bar"></div>
-                        <div class="label"></div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Username</label>
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                            name="name" autofocus>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="password2" class="d-block">Password Confirmation</label>
-                    <input id="password2" type="password"
-                        class="form-control @error('password_confirmation') is-invalid @enderror"
-                        name="password_confirmation">
-                    @error('password_confirmation')
-                        <div class="invalid-feedback">
-                            {{ $message }}
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email">
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password" class="d-block">Password</label>
+                        <input id="password" type="password" class="form-control pwstrength"
+                            data-indicator="pwindicator" name="password">
+                        <div id="pwindicator" class="pwindicator">
+                            <div class="bar"></div>
+                            <div class="label"></div>
                         </div>
-                    @enderror
-                </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password2" class="d-block">Password Confirmation</label>
+                        <input id="password2" type="password"
+                            class="form-control @error('password_confirmation') is-invalid @enderror"
+                            name="password_confirmation">
+                        @error('password_confirmation')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block">
-                        Register
-                    </button>
-                </div>
-            </form>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">
+                            Register
+                        </button>
+                    </div>
+                    <div class="mt-2 text-muted text-center">
+                        Already have an account? <a href="{{ route('login') }}">Login</a>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="mt-5 text-muted text-center">
-            Already have an account? <a href="{{ route('login') }}">Login</a>
-        </div>
-    </div>
-@endsection
+    </x-slot>
 
-@push('scripts')
-    <!-- JS Libraies -->
-    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
-    <script src="{{ asset('library/jquery.pwstrength/jquery.pwstrength.min.js') }}"></script>
+    @push('scripts')
+        <!-- JS Libraies -->
+        <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+        <script src="{{ asset('library/jquery.pwstrength/jquery.pwstrength.min.js') }}"></script>
 
-    <!-- Page Specific JS File -->
-    <script src="{{ asset('js/page/auth-register.js') }}"></script>
-@endpush
+        <!-- Page Specific JS File -->
+        <script src="{{ asset('js/page/auth-register.js') }}"></script>
+    @endpush
+</x-auth-layout>
